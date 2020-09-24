94440170bb5f974f64d52d39fdf2f0c494fcff5f
xiiethan <ethan_dillinger458@yahoo.com>
2020-09-24 22:18:09 +0000

    here

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ var app = express();
 //get home page
 app.get('/', function(req, res){
     //return something to homepage
-    res.send('Hello World');
+    //res.send('Hello World');
+    res.render('index');
 })
+
+//add post method /addtask
+app.post('/addtask', function(req, res){
+    //return index
+    res.render('index');
+}
 
 //server setup
 app.listen(3000, function(){
